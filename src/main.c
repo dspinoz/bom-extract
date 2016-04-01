4e37cecdff3d6a90aa32c9995307ccc3e784609e
Daniel Spinozzi <dspinoz@gmail.com>
2016-04-02 00:30:27 +1030

    compiles but cannot run

--- src/main.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-#include "common/common.h"
-
-int main()
-{
-  print(stdout, "Hello World");
-  return 0;
-}
---
+++ src/main.cpp
@@ -0,0 +1,70 @@
+#include <stdio.h>
+
+#include <tesseract/baseapi.h>
+#include <leptonica/allheaders.h>
+
+#include "common/common.h"
+
+int main(int argc, char **argv)
+{
+  fprintf(stdout, "Hello World");
+  
+  const char *language = "eng";
+  const char *datapath = "../tessdata/";
+  const char* filename = NULL;
+  PIX  *pix;
+
+  tesseract::TessBaseAPI *tAPI = new tesseract::TessBaseAPI();
+
+  printf("Tesseract-ocr version: %s\n",
+         tesseract::TessBaseAPI::Version());
+  printf("Leptonica version: %s\n",
+         getLeptonicaVersion());
+
+  if (argv[1] == NULL) {
+      filename = "../phototest.tif";
+  } else {
+      filename = argv[1];
+  }
+
+  // Create pix
+  FILE *fp = fopen(filename, "r");
+  if (fp) {
+    fclose(fp);
+    pix = pixRead(filename);
+    if (pix == NULL) {
+        printf("Unsupported image type.\n");
+        exit(3);
+    }
+    else {
+        printf("Pix created\n");
+    }
+  } else {
+    printf("file '%s' doesn't exist.\n", filename);
+    exit(3);
+  }
+
+  // Initalize tesseract
+  int rc = tAPI->Init(datapath, language, tesseract::OEM_DEFAULT, NULL, 0, NULL, NULL, false);
+  if (rc) {
+    fprintf(stderr, "Could not initialize tesseract.\n");
+    exit(1);
+  }
+
+  // Set PIX for OCR
+  tAPI->SetImage(pix);
+
+  // Optional: set rectangle to OCR
+  tAPI->SetRectangle(30, 90, 600, 173);
+
+  // run ocr
+  char* outText = tAPI->GetUTF8Text();
+  printf("OCR output:\n\n");
+  printf(outText);
+
+  tAPI->Clear();
+  tAPI->End();
+  delete [] outText;
+  pixDestroy(&pix);
+  return 0;
+}
